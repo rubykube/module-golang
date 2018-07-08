84862163d75cfbc8a4dc143996afb0c2fee60955
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-07-09 01:19:01 +0300

    Added first exercises

---
+++ ex1/README.md
@@ -0,0 +1,9 @@
+# Downcase
+
+Implement function that returns the string in lower case.(without using ```strings``` package)
+
+# Example
+
+``` input:    "Hello World"
+    expected: "hello world"
+```
---
+++ ex1/downcase_test.go
@@ -0,0 +1,60 @@
+package downcase
+
+import (
+	"testing"
+)
+
+var testCases = []struct {
+	input    string
+	expected string
+}{
+	{
+		input:    "",
+		expected: "",
+	},
+	{
+		input:    "Hello World",
+		expected: "hello world",
+	},
+	{
+		input:    "HeloiS",
+		expected: "helios",
+	},
+	{
+		input:    "barong is oauth server for peatio.tech stack.",
+		expected: "barong is oauth server for peatio.tech stack.",
+	},
+	{
+		input:    "tag 2.0.34",
+		expected: "tag 2.0.34",
+	},
+	{
+		input:    "CryptoCurrency Exange PEATIO",
+		expected: "cryptocurrency exange peatio",
+	},
+}
+
+func TestBracket(t *testing.T) {
+	for _, tt := range testCases {
+		actual, err := Downcase(tt.input)
+		// We don't expect errors for any of the test cases
+		if err != nil {
+			t.Fatalf("Downcase(%q) returned error %q.  Error not expected.", tt.input, err)
+		}
+		if actual != tt.expected {
+			t.Fatalf("Downcase(%q) was expected to return %v but returned %v.",
+				tt.input, tt.expected, actual)
+		}
+	}
+}
+
+func BenchmarkDowncase(b *testing.B) {
+	b.StopTimer()
+	for _, tt := range testCases {
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			Downcase(tt.input)
+		}
+		b.StopTimer()
+	}
+}
