e77d8d03d2d905f50367d581738f764eff455f03
dmytro <dmytroyesin@gmail.com>
2018-07-13 10:58:54 +0300

    ex03

--- ex03/stack.go
+++ ex03/stack.go
@@ -21,15 +21,20 @@ func (c *Stack) Push(numb int) {
 func (c *Stack) Pop() int {
 	c.i--
 	res := c.st[c.i]
+	c.st = c.st[:c.i]
 	return res
 }
 
 /*func main() {
-	s := make(stack, 0)
-	s = c.Push(1)
-	s = c.Push(2)
-	s = c.Push(3)
+	var c *Stack = New()
+	//	s := make(stack, 0)
+	c.Push(1)
+	c.Push(2)
+	c.Push(3)
 
 	p := c.Pop()
 	fmt.Println(p)
+	c.Push(4)
+	d := c.Pop()
+	fmt.Println(d)
 }*/
