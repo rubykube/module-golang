0e55a156656c1b6158923f1ba09dfafc191927e6
Egor Kovetskiy <e.kovetskiy@gmail.com>
2018-07-26 17:12:12 +0300

    ex07: rework task

--- ex07/README.md
+++ ex07/README.md
@@ -1,15 +1,52 @@
-# Goroutines
+# Workers
 
-Implement a function `Process` that returns channel of string(output channel) and takes a channel of string(input channel) as an argument.
+Implement a Goroutines pool manager that controls maximum number of running
+goroutines in pool.
 
-*Run two goroutines in the function:*
-- One is to perform operations on the strings received from the input channel and then write these strings to the output channel.
-- Other one is to close the output channel after receiving all the objects from input channel.
+Scheduler will read input data from stdin in interactive mode, scheduler must
+convert each line of input to float, spawn worker (goroutine), and
+sleep specified number of seconds in spawned goroutine.
 
+The maximum number of workers must be limited by variable that passed to
+function.
 
- *Operations performed by the first goroutine:*
-1. Put string into parentheses.
-for example:
-`hello` will be converted to `(hello)`
-`hello world !!` will be converted to `(hello world !!)`
-`(hello)` will be converted to `((hello))`
+Let's say we run scheduler with max_workers=10, it means that scheduler can
+spawn maximum 10 goroutines, but it must not spawn them just right after
+start, only if there is a work to be done.
+
+If number of received tasks is less than 10 then number of running
+goroutines must be less than 10 too.
+
+If user sent only 5 tasks, then number of running workers must be 5 only.
+
+If user sent 12 tasks, then number of running workers must be 10 (because
+max_workers=10) and then existing workers must be re-used for finishing last 2
+tasks, and free 8 workers must be stopped because there are no tasks to be
+done.
+
+## Output
+
+* Before spawning new worker scheduler must print message: `worker:$worker_id spawning`
+* After starting executing new task worker must print message: `worker:$worker_id sleep:$sleep_seconds`
+* Before stopping worker goroutine scheduler must print message: `worker:$worker_id stopping`
+
+    Where $worker_id is number of worker starting from 1.
+    Where $sleep_seconds is given number of seconds to sleep.
+
+# Level: advanced
+
+Provide HTTP API /stats and expose number of running goroutines at the time.
+
+# Level: graceful
+
+Scheduler must support graceful shutdown, it means that if daemon received
+SIGTERM/SIGINT linux process signal it should stop accepting new tasks from
+stdin (just close stdin file descriptor), wait for running tasks and stop
+the process.
+
+# Level: serious
+
+The maximum number of workers will be specified in file and scheduler must
+support reloading configuration in real-time, it means that if daemon received
+SIGHUP linux process signal it should re-load configuration file and adjust
+number of maximum running goroutines.
--- ex07/goroutines_test.go
+++ ex07/goroutines_test.go
@@ -1,62 +1,231 @@
 package goroutines
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var testCases = []struct {
-	input    string
-	expected string
-}{
-	{
-		input:    "",
-		expected: "()",
-	},
-	{
-		input:    "Hello World",
-		expected: "(Hello World)",
-	},
-	{
-		input:    "tag 2.0.68",
-		expected: "(tag 2.0.68)",
-	},
-	{
-		input:    "Hey !! How are you?",
-		expected: "(Hey !! How are you?)",
-	},
-	{
-		input:    "(Hello World!!)",
-		expected: "((Hello World!!))",
-	},
-	{
-		input:    "12345",
-		expected: "(12345)",
-	},
+var (
+	testcases = []struct {
+		MaxThreads  int
+		Input       []string
+		Output      []string
+		WaitSeconds int
+	}{
+		//{
+		//    2,
+		//    []string{
+		//        "0.1",
+		//    },
+		//    []string{
+		//        "worker:1 spawning",
+		//        "worker:1 sleep:0.1",
+		//        "worker:1 stopping",
+		//    },
+		//    1,
+		//},
+
+		//{
+		//    2,
+		//    []string{
+		//        "0.1",
+		//        "0.2",
+		//    },
+		//    []string{
+		//        "worker:1 spawning",
+		//        "worker:1 sleep:0.1",
+		//        "worker:2 spawning",
+		//        "worker:2 sleep:0.2",
+		//        "worker:1 stopping",
+		//        "worker:2 stopping",
+		//    },
+		//    1,
+		//},
+
+		//{
+		//    1,
+		//    []string{
+		//        "0.1",
+		//        "0.2",
+		//    },
+		//    []string{
+		//        "worker:1 spawning",
+		//        "worker:1 sleep:0.1",
+		//        "worker:1 sleep:0.2",
+		//        "worker:1 stopping",
+		//    },
+		//    1,
+		//},
+
+		{
+			10,
+			[]string{
+				"0.2",
+				"0.3",
+				"0.4",
+				"0.5",
+				"0.6",
+				"0.7",
+				"0.8",
+				"0.9",
+				"1.0",
+				"1.1",
+				"1.2",
+				"1.3",
+				"1.4",
+			},
+			[]string{
+				"worker:1 spawning",
+				"worker:1 sleep:0.2",
+				"worker:2 spawning",
+				"worker:2 sleep:0.3",
+				"worker:3 spawning",
+				"worker:3 sleep:0.4",
+				"worker:4 spawning",
+				"worker:4 sleep:0.5",
+				"worker:5 spawning",
+				"worker:5 sleep:0.6",
+				"worker:6 spawning",
+				"worker:6 sleep:0.7",
+				"worker:7 spawning",
+				"worker:7 sleep:0.8",
+				"worker:8 spawning",
+				"worker:8 sleep:0.9",
+				"worker:9 spawning",
+				"worker:9 sleep:1.0",
+				"worker:10 spawning",
+				"worker:10 sleep:1.1",
+				"worker:1 sleep:1.2",
+				"worker:2 sleep:1.3",
+				"worker:3 sleep:1.4",
+				"worker:4 stopping",
+				"worker:5 stopping",
+				"worker:6 stopping",
+				"worker:7 stopping",
+				"worker:8 stopping",
+				"worker:9 stopping",
+				"worker:10 stopping",
+				"worker:1 stopping",
+				"worker:2 stopping",
+				"worker:3 stopping",
+			},
+			3,
+		},
+	}
+)
+
+func TestRun(t *testing.T) {
+	test := assert.New(t)
+
+testcases:
+	for testID, testcase := range testcases {
+		log.Printf("start testcase: #%d", testID)
+
+		stdinReader, stdinWriter, err := os.Pipe()
+		if err != nil {
+			panic(err)
+		}
+
+		stdoutReader, stdoutWriter, err := os.Pipe()
+		if err != nil {
+			panic(err)
+		}
+
+		os.Stdin = stdinReader
+		os.Stdout = stdoutWriter
+
+		runner := testRun(testcase.MaxThreads)
+
+		for _, line := range testcase.Input {
+			log.Printf("    > %s", line)
+
+			_, err := stdinWriter.WriteString(line + "\n")
+			if err != nil {
+				test.NoError(
+					err,
+					fmt.Sprintf("testcase: %d, can't write to stdin", testID),
+				)
+				break testcases
+			}
+
+			// In real world user will take some time to think about next
+			// number, it also helps to skip concurrency hell
+			time.Sleep(time.Millisecond * 10)
+		}
+
+		err = stdinWriter.Close()
+		if err != nil {
+			panic(err)
+		}
+
+		scanner := bufio.NewScanner(stdoutReader)
+		for number, expected := range testcase.Output {
+			test.True(
+				scanner.Scan(),
+				fmt.Sprintf("testcase: %d, can't read stdout", testID),
+			)
+
+			actual := scanner.Text()
+
+			log.Printf("    < %s", actual)
+
+			test.Equal(
+				expected,
+				actual,
+				fmt.Sprintf(
+					"testcase: %d, output (line: %d) validation",
+					testID,
+					number+1,
+				),
+			)
+
+		}
+
+		wait(test, testID, runner, time.Duration(testcase.WaitSeconds)*time.Second)
+	}
 }
 
-func TestProcess(t *testing.T) {
-	for _, tt := range testCases {
-		// GIVEN
-		input := make(chan string)
-		defer close(input)
+func testRun(poolSize int) *sync.WaitGroup {
+	runner := &sync.WaitGroup{}
+	runner.Add(1)
 
-		done := make(chan bool)
-		defer close(done)
+	go func() {
+		defer runner.Done()
+		Run(poolSize)
+	}()
 
-		go func() {
-			input <- tt.input
-			done <- true
-		}()
+	return runner
+}
 
-		// WHEN
-		output := Process(input)
-		<-done // blocks until the input write routine is finished
+func wait(
+	test *assert.Assertions,
+	testID int,
+	runner *sync.WaitGroup,
+	maxTime time.Duration,
+) {
+	done := make(chan struct{})
+	go func() {
+		runner.Wait()
+		done <- struct{}{}
+	}()
 
-		// THEN
-		found := <-output // blocks until the output has contents
-
-		if found != tt.expected {
-			t.Errorf("Expected %s, found %s", tt.expected, found)
-		}
+	after := time.After(maxTime)
+	select {
+	case <-after:
+		test.FailNowf(
+			"runner was not stopped",
+			"testcase: %d operation timed out after %s",
+			testID,
+			maxTime,
+		)
+	case <-done:
+		return
 	}
 }
